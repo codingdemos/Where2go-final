0a1793c8f0af107eee6beea5fee160b7c66fb2c0
CodingDemos <codingdemos@gmail.com>
2018-12-26 07:31:59 +0800

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/where2go/MainActivity.kt
@@ -0,0 +1,314 @@
+/*
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.where2go
+
+import android.app.Activity
+import android.content.Intent
+import android.location.Location
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.Marker
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity(), PermissionsListener,
+    MapboxMap.OnMapClickListener, LocationEngineListener, OnMapReadyCallback {
+
+    val REQUEST_CHECK_SETTINGS = 1
+
+    lateinit var mapView: MapView
+    lateinit var map: MapboxMap
+    lateinit var permissionManager: PermissionsManager
+    lateinit var originLocation: Location
+    lateinit var navigate: FloatingActionButton
+    lateinit var startPoint: Point
+    lateinit var endPoint: Point
+
+    var locationEngine: LocationEngine? = null
+    var locationComponent: LocationComponent? = null
+    var destinationMarker: Marker? = null
+    var navigationMapRoute: NavigationMapRoute? = null
+    var currentRoute: DirectionsRoute? = null
+    var settingsClient: SettingsClient? = null
+    var locationRequestBuilder: LocationSettingsRequest.Builder? = null
+    var locationRequest: LocationSettingsRequest? = null
+    var resolvableException: ResolvableApiException? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.access_token))
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapbox)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        settingsClient = LocationServices.getSettingsClient(this)
+
+
+        navigate = findViewById(R.id.btnNavigate)
+        navigate.isEnabled = false
+        navigate.setOnClickListener {
+            val navigationLauncherOptions = NavigationLauncherOptions.builder()
+                .directionsRoute(currentRoute)
+                .shouldSimulateRoute(true)
+                .build()
+            NavigationLauncher.startNavigation(this, navigationLauncherOptions)
+        }
+
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        map = mapboxMap!!
+        locationRequestBuilder = LocationSettingsRequest.Builder().addLocationRequest(
+            LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        )
+        locationRequest = locationRequestBuilder?.build()
+        settingsClient?.checkLocationSettings(locationRequest)
+            ?.addOnSuccessListener {
+                enableLocation()
+            }
+            ?.addOnFailureListener {
+                val statusCode = (it as ApiException).statusCode
+                if (statusCode == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                    resolvableException = it as ResolvableApiException
+                    resolvableException?.startResolutionForResult(this, REQUEST_CHECK_SETTINGS);
+
+                }
+            }
+        map.addOnMapClickListener(this)
+    }
+
+    fun enableLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            initializeLocationComponent()
+            initializeLocationEngine()
+        } else {
+            permissionManager = PermissionsManager(this)
+            permissionManager.requestLocationPermissions(this)
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    fun initializeLocationEngine() {
+        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.activate()
+        locationEngine?.addLocationEngineListener(this)
+
+
+        val lastLocation = locationEngine?.lastLocation
+        if (lastLocation != null) {
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        } else {
+            locationEngine?.addLocationEngineListener(this)
+        }
+
+    }
+
+    fun setCameraPosition(location: Location) {
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 30.0))
+    }
+
+    @SuppressWarnings("MissingPermission")
+    fun initializeLocationComponent() {
+        locationComponent = map.locationComponent
+        locationComponent?.activateLocationComponent(this)
+        locationComponent?.isLocationComponentEnabled = true
+        locationComponent?.cameraMode = CameraMode.TRACKING
+
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        location?.let {
+            originLocation = location
+            setCameraPosition(location)
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    override fun onConnected() {
+        locationEngine?.requestLocationUpdates()
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, getString(R.string.msg_map_permission), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocation()
+        } else {
+            Toast.makeText(this, getString(R.string.msg_location_not_granted), Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                enableLocation()
+            } else
+                if (resultCode == Activity.RESULT_CANCELED) {
+                    finish()
+                }
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    override fun onStart() {
+        super.onStart()
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            locationEngine?.requestLocationUpdates()
+            locationComponent?.onStart()
+        }
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        locationEngine?.removeLocationUpdates()
+        locationComponent?.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+        locationEngine?.deactivate()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        if (outState != null) {
+            mapView.onSaveInstanceState(outState)
+        }
+    }
+
+    override fun onMapClick(point: LatLng) {
+
+        destinationMarker?.let {
+            map.removeMarker(it)
+        }
+
+        destinationMarker = map.addMarker(MarkerOptions().position(point))
+        startPoint = Point.fromLngLat(originLocation.longitude, originLocation.latitude)
+        endPoint = Point.fromLngLat(point.longitude, point.latitude)
+
+        getRoute(startPoint, endPoint)
+    }
+
+    fun getRoute(originPoint: Point, endPoint: Point) {
+        NavigationRoute.builder(this)
+            .accessToken(Mapbox.getAccessToken()!!)
+            .origin(originPoint)
+            .destination(endPoint)
+            .build()
+            .getRoute(object : Callback<DirectionsResponse> {
+                override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                    Log.d("MainActivity", t.localizedMessage)
+                }
+
+                override fun onResponse(
+                    call: Call<DirectionsResponse>,
+                    response: Response<DirectionsResponse>
+                ) {
+
+                    if (navigationMapRoute != null) {
+                        navigationMapRoute?.removeRoute()
+                    } else {
+                        navigationMapRoute = NavigationMapRoute(null, mapView, map)
+                    }
+
+                    currentRoute = response.body()?.routes()?.first()
+                    if (currentRoute != null) {
+                        navigationMapRoute?.addRoute(currentRoute)
+                        navigate.isEnabled = true
+                    }
+                }
+
+            })
+    }
+
+}
